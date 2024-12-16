744b5a15917855afb9d315211ba9f4e818ac3883
Huy <ndkhuy7694@gmail.com>
2024-12-16 21:45:34 +0700

    fix linting

--- udagram/udagram-frontend/src/app/api/api.service.ts
+++ udagram/udagram-frontend/src/app/api/api.service.ts
@@ -57,8 +57,9 @@ export class ApiService {
   async upload(endpoint: string, file: File, payload: any): Promise<any> {
     const signed_url = (await this.get(`${endpoint}/signed-url/${file.name}`)).url;
 
-    const headers = new HttpHeaders({'Content-Type': file.type, 
-                                    'Authorization': `jwt ${this.token}`});
+    const headers = new HttpHeaders({'Content-Type': file.type,
+                                     'Authorization': `jwt ${this.token}`
+                                    });
     const req = new HttpRequest( 'PUT', signed_url, file,
                                   {
                                     headers: headers,
